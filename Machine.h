8570e00bfc29a8a0e61cf3425773915d4cef4ea9
Eddie Seung Hun Han <44196834+SpartanEngineer247@users.noreply.github.com>
2020-03-12 13:46:17 +0400

    Update and rename MachinesClass to Machine.h

--- Machine.h
+++ Machine.h
@@ -8,17 +8,17 @@
 #include<iostream>
 using namespace std;
 
-//Base class
+//Base class for the Machine objects
 class Machine
 {
 protected:
 	string name;	//name of machine
 	bool status;	//availability of machine - if false, machine is available to use
 	//setting to the 'minutes of operation' the machine is operating with right now. Is -1 if the machine is not operating, -2 if broken
-	int currentfunction;
-	int time;
+	int currentfunction; //The functionality of the machine itself (how many minutes of operation,etc...)
+	int time; //Time left of the machine's operation - this isn't used properly as the asynchronous timer is currently not fully functional
 public:
-	//default constructor
+	//default constructor, not necessary but placed just in case it throws errors
 	Machine()
 	{
 		name = "NA";
@@ -26,7 +26,7 @@ public:
 		currentfunction = -1;
 		time = 0;
 	}
-	//preferred constructor - the inputs are ideally taken from file information
+	//preferred constructor - the inputs are ideally taken from the memory file
 	Machine(string newname, bool currentstatus, int newop, int newtime)
 	{
 		name = newname;
@@ -63,7 +63,7 @@ public:
 	//abstract function, should print out the operational information of the machine
 	virtual void printoperation() = 0;
 
-
+	//Function which calls the asynchronous timer block and stores it into an 'auto' a variable
 	void starttimer(int s, string machinename)
 	{
 		auto a = async(timer, s, machinename);
@@ -74,9 +74,10 @@ class WashingMachine : public Machine
 {
 private:
 	//hardcoded some 'minutes of operation' of a washing machine - this may be updated later to be able to read from file
+	//While throughout the code it is written as 'minutes' for demonstration purposes these numbers serve as seconds
 	int machinefunction[5] = { 10, 20, 25, 30, 50 };
 public:
-	//constructors - pretty self explanatory
+	//constructors
 	WashingMachine() :Machine() { currentfunction = -1; }
 	WashingMachine(string inname, bool currentstatus, int currentsetting, int newtime) :Machine(inname, currentstatus, currentsetting, newtime){}
 
@@ -96,6 +97,7 @@ public:
 	}
 
 	//prints the operating status of the machine right now
+	//this is not fully implemented as the timer function locks the program out of further operation
 	void printoperation()
 	{
 		cout << endl << "Washing Machine " << name;
@@ -112,6 +114,9 @@ public:
 		}
 	}
 
+	//function that returns the number of minutes the setting indicates (i.e. setting 3 desires 25 minutes)
+	//This function is shared in terms of functionality with dryingmachine but are implemented separately because
+	//Each machine type has unique functionality settings - this was a behavior of the program that hasn't been fully realized
 	int getminutes(int setting)
 	{
 		return machinefunction[setting];
@@ -180,6 +185,7 @@ public:
 		return needcleaning;
 	}
 
+	//same function as getminutes in washingmachines class
 	int getminutes(int setting)
 	{
 		return machinefunction[setting];
