7c23f51561b155f0df2ff73982f543ffa4325e36
Eddie Seung Hun Han <44196834+SpartanEngineer247@users.noreply.github.com>
2020-03-12 13:48:00 +0400

    Update and rename UsersTableClass to User_Database.h

--- User_Database.h
+++ User_Database.h
@@ -9,16 +9,25 @@
 #include<iostream>
 using namespace std;
 
+//Users_Database is a slight misnomer - this class creates a singleton overhead object that manages the entire database
 class Users_Database
 {
 private:
+	//Users_Database pointer, which stores the location of the database object
 	static Users_Database *instance;
+
+	//Stores the currently logged in user's name
 	string currentuser;
 
+	//Default constructor, called through GetInstance public function
 	Users_Database()
 	{
 		currentuser = "";
 
+		//Opens memory files, "usersmemory.txt", "washingmachinesmemory.txt", "dryingmachinesmemory.txt"
+		//and attempts to take information stored within
+		//The program currently is UNABLE to handle corrupted data (i.e. incomplete information)
+		//However it is self-sufficient in that it will handle saving and loading memory safely if there is no corruption
 		ifstream inFileBuffer("usersmemory.txt");
 		if (inFileBuffer.fail())
 		{
@@ -78,12 +87,19 @@ private:
 		}
 		inFileBuffer3.close();
 	}
+	//FOR REFERENCE
+	//usersmemory.txt stores username, password (NOT HASHED BUT IN PLAINTEXT, SECURITY ISSUE), and number of machines used
+	//washingmachinesmemory.txt stores machine name, it's availability, it's current function, and the time left of its operation
+	//dryingmachinesmemory.txt stores machine name, it's availability, it's current function, and the time left of its operation
 
+	//Three maps stores the data from the memory - the keys are the names, and the second elements are pointers to objects
+	//that has been dynamically assigned to memory spaces
 	map<string, User*> Table;
 	map<string, WashingMachine*> WashingTable;
 	map<string, DryingMachine*> DryingTable;
 
 public:
+	//Singleton construction call
 	static Users_Database* GetInstance()
 	{
 		if (instance == nullptr)
@@ -93,6 +109,8 @@ public:
 		return instance;
 	}
 	
+
+	//Default destructor, essential for storing the session information properly back into the memory files
 	~Users_Database()
 	{
 		ofstream outFileBuffer("usersmemory.txt");
@@ -143,10 +161,10 @@ public:
 			map<string, DryingMachine*>::iterator it;
 			for (it = DryingTable.begin(); it != DryingTable.end(); it++)
 			{
-                		outFileBuffer3 << it->first << " ";
-                		outFileBuffer3 << it->second->getstatus() << " ";
-                		outFileBuffer3 << it->second->getcurrentfunction() << " ";
-                		outFileBuffer3 << it->second->getoperations() << endl;
+				outFileBuffer3 << it->first << " ";
+				outFileBuffer3 << it->second->getstatus() << " ";
+				outFileBuffer3 << it->second->getcurrentfunction() << " ";
+				outFileBuffer3 << it->second->getoperations() << endl;
 			}
 		}
 		outFileBuffer3.close();
@@ -158,6 +176,8 @@ public:
 		instance = nullptr;
 	}
 
+	//Function that takes in user's name and designated password and checks if the credentials are correct
+	//If so, the program will move on to next functionalities and will set the session to the inputID
 	bool Login(string inputnetID, string &inputpassword)
 	{
 		map<string, User*>::iterator it;
@@ -177,6 +197,9 @@ public:
 		currentuser = inputnetID;
 	}
 	
+	//Function that is called to create a new user by taking in a new ID and password and creates a new object of a user
+	//It then stores the pointer to this this newly created user into the Users map
+	//If the user already exists, it simply throws an error and does not store the new information
 	bool CreateNewUser(string &newnetID, string &newpassword)
 	{
 		map<string, User*>::iterator it;
@@ -195,6 +218,8 @@ public:
 		}
 	};
 
+	//Function which takes in userid and password and first attempts to login, and if successful
+	//it will delete the user from the map of the users
 	bool DeleteNewUser(string &newnetID, string &newpassword)
 	{
 		if (Login(newnetID, newpassword) != true)
@@ -212,18 +237,24 @@ public:
 		}
 	};
 
+	//Logout function that clears the name of the current session user
+	//THIS FUNCTIONALITY IS UNUSED BUT IS PROGRAMMED IN TO ANTICIPATE THE PROGRAM BEING INITIATED IN MULTIPLE SEPARATE INSTANCES
 	void logout()
 	{
 		currentuser = "";
 	}
 
+	//A function that checks the status of the machine given
+	//This function is identical to checkdryingmachinestatus
+	//Whilst it is possible to have a general checkmachinestatus (this is actually done elsewhere)
+	//The way the main calls on this function allows this function to have a limited scope
 	bool checkwashingmachinestatus(string machinename)
 	{
 		map<string, WashingMachine*>::iterator it;
 		it = WashingTable.find(machinename);
 		return (it->second->getstatus());
 	}
-
+	//Identical to checkwashingmachinestatus, but for drying machines
 	bool checkdryingmachinestatus(string machinename)
 	{
 		map<string, DryingMachine*>::iterator it;
@@ -231,6 +262,9 @@ public:
 		return (it->second->getstatus());
 	}
 
+	//Changes washing machine status based on the chosen machine functionality
+	//Prints out a message to confirm that the machine is now in use
+	//It also calls the timer function
 	void changewashingmachinestatus(string machinename, int newsetting)
 	{
 		map<string, WashingMachine*>::iterator it;
@@ -241,6 +275,9 @@ public:
 		it->second->starttimer(it->second->getminutes(newsetting),machinename);
 	}
 
+	//Changes drying machine status based on the chosen machine functionality
+	//Prints out a message to confirm that the machine is now in use
+	//It also calls the timer function
 	void changedryingmachinestatus(string machinename, int newsetting)
 	{
 		map<string, DryingMachine*>::iterator it;
@@ -251,21 +288,24 @@ public:
 		it->second->starttimer(it->second->getminutes(newsetting),machinename);
 	}
 
+	//General function call to turn off the machine (set its functionality to -1 and mark it as available to use
+	//Shows the simple if-else statement that could have been implemented in the 'check' and 'change' functions
 	void turnoffmachine(string machinename)
 	{
 		map<string, DryingMachine*>::iterator it;
-        	it = DryingTable.find(machinename);
-        	if (it == DryingTable.end())
-        	{
-        	    map<string, WashingMachine*>::iterator it2;
-        	    it2 = WashingTable.find(machinename);
-        	    it2->second->changestatus(-1);
-        	}
-        	else
-        	{
-        	    it->second->changestatus(-1);
-        	}
+		it = DryingTable.find(machinename);
+		if (it == DryingTable.end())
+		{
+			map<string, WashingMachine*>::iterator it2;
+			it2 = WashingTable.find(machinename);
+			it2->second->changestatus(-1);
+		}
+		else
+		{
+			it->second->changestatus(-1);
+		}
 	}
 };
 
+//Initates the static pointer for the singleton
 Users_Database* Users_Database::instance = nullptr;
